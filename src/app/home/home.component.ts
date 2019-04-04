660312d9284ec3232cd755f74ee72483f24367ea
Uppala <shivatej-goud.uppala@capgemini.com>
2019-04-04 09:52:15 -0400

    new implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -12,6 +13,30 @@ export class HomeComponent implements OnInit {
   isDriverButton:boolean = false;
   isPassengerButton:boolean = false;
   nextEnable:boolean = false;
+  step:number = 1;
+  pg2PolicyClick:boolean = false;
+  listMenu:boolean = false;
+  pg3Continue:boolean = false;
+  pg6Continue: boolean = false;
+  uploaded3rdparty: boolean = false;
+  uploadedpolice:boolean = false;
+  uploadedcar:boolean = false;
+  pg6no:boolean = false;
+  homeAccbtn:boolean = false;
+  accSitebtn:boolean = false;
+  bodyShpbtn:boolean = false;
+  pg7Continue:boolean = false;
+  policyNumber:string ="";
+  userSelectReport: string;
+  model:string;
+  modelDOB:string;
+  time:string;
+  lastName:string;
+  firstName:string;
+  incidentDesc:string;
+  zipCode:string;
+  phoneNumber:string;
+  email:string;
 
   constructor(private modalService: NgbModal) { }
   
@@ -22,12 +47,48 @@ export class HomeComponent implements OnInit {
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title',centered: true}).result.then((result) => {
     }, (reason) => {
-      
     });
   }
 
-  buttonStatus(){
+  nextPage(){
+    this.step++;
+  }
+  homePage(){
+    this.step =1;
+  }
+  toggleMenu(){
+    this.listMenu = !this.listMenu;
+  }
+  revertPage(){
+    this.step = 8;
+  }
+  preSubmissionPage(){
+    this.step = 11;
+  }
+  prevPage(){
+    this.step--;
+  }
 
+  checkUserDetails(){
+    console.log(this.modelDOB,"324");
+    if( this.firstName && this.lastName && this.modelDOB){
+      this.pg2PolicyClick = true;
+    }
+  }
+  checkAccDetails(){
+    if( this.time && this.model && this.incidentDesc){
+      this.pg6Continue = true;
+    }
+  }
+
+  checkAccidentDetails(){
+    if( this.zipCode && (this.phoneNumber || this.email)){
+      this.pg7Continue = true;
+    }
+  }
+
+  checkdate(){
+    console.log('eer',this.model);
   }
 
 }
