b2e695f4eb5ec81d675cc00434b8af97d9651be5
Uppala <shivatej-goud.uppala@capgemini.com>
2019-04-12 17:57:18 -0400

    testing with server js

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ res.sendFile(path.join(__dirname+'/dist/digital/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(server_port, server_host, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
