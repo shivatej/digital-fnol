8ee0eb1d43b9cf53360e411d6d04b60fcdc824d3
Uppala <shivatej-goud.uppala@capgemini.com>
2019-04-12 17:29:16 -0400

    server port change

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const path = require('path');
 
 const app = express();
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/digital'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/digital/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
+app.listen(server_port, server_host, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
