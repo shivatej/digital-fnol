3f93e62c9bc3d8dfb9bc796772870053b8beac5e
Uppala <shivatej-goud.uppala@capgemini.com>
2019-04-16 00:08:40 -0400

    changes in server

--- server.js
+++ server.js
@@ -13,4 +13,7 @@ res.sendFile(path.join(__dirname,'/dist/digital/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+  });
