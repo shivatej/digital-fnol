cf72148b048079cc6ca6f8ee94e14152570dd653
arjvijay <49760622+arjvijay@users.noreply.github.com>
2019-06-18 16:17:29 +0530

    Update shared-service.service.ts

--- src/app/shared/shared-service.service.ts
+++ src/app/shared/shared-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpResponse, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,8 @@ import { Observable, Subject } from 'rxjs';
 export class SharedServiceService {
 
   private subject = new Subject<any>();
-  
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   sendHeading(step: number) {
     this.subject.next(step);
@@ -21,5 +22,104 @@ export class SharedServiceService {
   getHeading(): Observable<any> {
     return this.subject.asObservable();
   }
+
+  setIsLoggedIn(isLoggedIn: boolean) {
+    this.subject.next(isLoggedIn);
+  }
+
+  getIsLoggedIn(): Observable<any> {
+    return this.subject.asObservable();
+  }
+
+  initializeApp(): Promise<any> {
+
+    let headers = {
+      'accept': 'application/json',
+      'Connection': 'keep-alive',
+      'X-CSRF-Token': 'Fetch',
+      'Access-Control-Allow-Headers': 'x-auth, content-type'
+    }
+
+    let options = { headers: headers };
+
+    // let url = ConfigConstants.clientUrl + 'oshot/oauth/post';
+    let url = 'http://localhost:3001/upload?url=oshot/oauth/post';
+    let reqData = {
+      "vehicles": {
+        "insurer_id": 2,
+        "veh_make": "honda",
+        "veh_model": "accord",
+        "veh_year": 2009
+      },
+      "events": {
+        "evt_customer": 12067,
+        "evt_country": "India",
+        "evt_zipCode": "02109",
+        "evt_createDate": "2018-01-15T22:10:42"
+      }
+    };
+
+    return new Promise((resolve, reject) => {
+      this.http.post(url, reqData).subscribe((response: any) => {
+        console.log("Test....", response);
+        // console.log("Test....", response.headers.get('x-csrf-token'));
+        setTimeout(() => {
+          this.setIsLoggedIn(true);
+        });
+
+        resolve(true);
+      }, error => {
+        this.setIsLoggedIn(false);
+        console.error(error)
+      });
+    });
+  }
+
+  uploadDocument(uploadDocbody, imageChkSum) {
+    // const authorizedHeaders: HttpHeaders = this.createAuthorizationHeaders();
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    // return this.http.post(`url`, uploadDocbody);
+
+    let url = 'http://localhost:3001/upload?url=oshot/post';
+    let reqData = {
+      "vehicles": {
+        "veh_make": "Jeep Wrangler",
+        "veh_model": "782WER",
+        "veh_year": "2016"
+      },
+      "incidents": {
+        "idt_country": "US",
+        "idt_postalCode": "02210",
+        "idt_createDate": "2019-05-08T10:10:37"
+      },
+      "images": [{
+        "img_name": "image1.jpg",
+        "img_data": {
+          "img_checksum": imageChkSum,
+          "base64ByteStream": uploadDocbody
+        }
+      }]
+    };
+
+    return new Promise((resolve, reject) => {
+      this.http.post(url, reqData).subscribe((response: any) => {
+        console.log("Test....", response);
+
+        resolve(response);
+      }, error => {
+        this.setIsLoggedIn(false);
+        console.error(error)
+      });
+    });
+
+  }
+
+  // createAuthorizationHeaders() {
+  //   // let headers = new HttpHeaders({
+  //   //   'Content-Type' : 'application/json'
+  //   // });
+  //   const headers = new HttpHeaders().set('Content-Type', 'application/json');
+  //   return headers;
+  // }
 
 }
