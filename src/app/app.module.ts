9fb429171b12d3510b3f17b4ae68998241f6e494
Uppala <shivatej-goud.uppala@capgemini.com>
2019-06-18 21:13:52 -0400

    crc32 error fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule ,
+    HttpClientModule,
     NgbModule,
     FormsModule
   ],
